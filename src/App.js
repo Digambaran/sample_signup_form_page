73ac07fecb068f40df36efe3db90d0013147ca9c
Digambaran <arjunsreekumar.1@gmail.com>
2022-12-23 14:42:11 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Sample_signup_form_page from './sample_signup_form_page'
+export default function App() {
+  return (
+    <div>
+      <Sample_signup_form_page/>
+    </div>
+  )
+}
---
+++ src/sample_signup_form_page.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Sample_signup_form_page = () => {
+  return (
+   <p>Hello from sample_signup_form_page </p>
+  )
+}
+export default Sample_signup_form_page
